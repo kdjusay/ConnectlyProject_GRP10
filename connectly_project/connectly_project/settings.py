3c5a3052926230c9b0304f856189e73875142081
kdjusay <122866609+kdjusay@users.noreply.github.com>
2025-02-03 23:25:08 +0800

    Posts Update

--- connectly_project/connectly_project/settings.py
+++ connectly_project/connectly_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', #add  Django REST Framework
+    'posts',
 ]
 
 MIDDLEWARE = [
--- connectly_project/connectly_project/urls.py
+++ connectly_project/connectly_project/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to Connectly!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home),
     path('api-auth/', include('rest_framework.urls')), #DRF login/logout
+    path('posts/', include('posts.urls'))
 ]
